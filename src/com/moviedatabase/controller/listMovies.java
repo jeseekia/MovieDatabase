3e1adf8a49517c2c975f706a393511ade42ada61
Jeseekia Vaughn <jeseekia@gmail.com>
2016-03-04 23:15:32 -0500

    Beginning work on the controller. Adding the first request mapping for /list.

--- src/com/moviedatabase/controller/listMovies.java
+++ src/com/moviedatabase/controller/listMovies.java
@@ -1,6 +1,13 @@
 package com.moviedatabase.controller;
 
+import org.springframework.stereotype.Controller;
+
 @Controller
 public class listMovies {
+	
+	@RequestMapping("/list")
+	public ModelAndView listData() {
+		
+	}
 
 }
